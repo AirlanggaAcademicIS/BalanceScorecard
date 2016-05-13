bd34a7f5d4827d013832b0fdaa185460e94e6908
evydwi <evydwicahyati@gmail.com>
2016-05-13 14:28:38 +0700

    Update User Story : Monitoring
    Do : mengubah bentuk tulisan di dalam tabel
    To Do : mengubah desain tabel menjadi lebih interaktif
    Hambatan : -

--- monitoring.php
+++ monitoring.php
@@ -36,72 +36,81 @@ include "koneksi.php"
     </div>
   </div>
 </nav>
-  
+
+
+
 <div class="container-fluid text-center">
+
   <div class="row content">
-    <div class="col-sm-2 sidenav">
-    </div>
+    <div class="col-sm-2 sidenav">             
+		<img src="images/cinqueterre.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+	</div>
     <div class="col-sm-8 text-left"> 
-            <h2 class="text-center">Monitoring</h2>
-			<form action="" method="post">
-			<select name="tahun" style="width:200px; height:35px; border:1px dotted #333333; border-radius:4px; -moz-border-radius:4px; font-family:Garamond; font-size:24px; margin-left:10px;">
-			<option value="">--Pilih--</option>
-			<?php
-			$q2 = mysql_query("SELECT DISTINCT date_format(WAKTU_MULAI_PROKER, '%Y') as bulantahun FROM proker order by WAKTU_MULAI_PROKER ASC");
-			echo '<form method="POST" action="monitoring.php">';
-			while ($row2 = mysql_fetch_array($q2)){
-			echo '<option value="'.$row2['bulantahun'].'">'.$row2['bulantahun'].'</option>';
-			}
-			?>
-			</select>
-			<input type="submit" name="lihat" value="Lihat" style="width:75px; height:35px; border:1px dotted #333333; border-radius:4px; -moz-border-radius:4px; font-family:Garamond; font-size:24px; margin-left:10px;">
+		<h2 class="text-center">Monitoring</h2>
+			<form action="monitoring.php" method="post" >
+			<div class="form-group">
+				<label for="sel1">Pilih Tahun:</label>
+				<select name='tahun' class="form-control" id="sel1" style="width:200px; height:35px;" onChange='this.form.submit()'>
+				<option value="">--Pilih--</option>
+					<?php
+						$q2 = mysql_query("SELECT DISTINCT date_format(WAKTU_MULAI_PROKER, '%Y') as bulantahun FROM proker order by WAKTU_MULAI_PROKER ASC");
+						echo '<form method="POST" action="monitoring.php">';
+						while ($row2 = mysql_fetch_array($q2)){
+						if ($_POST['tahun'] == $row2['bulantahun'])
+							echo "<option value=".$row2['bulantahun']." selected>".$row2['bulantahun']."</option>";
+						else
+							echo "<option value=".$row2['bulantahun'].">".$row2['bulantahun']."</option>";}
+					?>
+      			</select>
+      <br>
+    </div>
 			</form>
-			<div class="box-body table-responsive">
-			<br></br>
-                <table id="statustabel" class="table table-bordered table-striped" style="background:#F9F9F9; font-size:18px; font-family:Geneva, Arial, Helvetica, sans-serif;" >
-                    <thead>
-                        <tr>
-							<td style="size:landscape; background-color:#C0C0C0;">Terlaksana</td>
-						</tr>
-						<tr>
-							<td>
-							<?php 
-							if(isset($_POST['lihat'])){
-							$tahun = $_POST['tahun'];
-							$query = mysql_query("SELECT * FROM proker WHERE STATUS_PROKER='Terlaksana' AND date_format(WAKTU_MULAI_PROKER, '%Y') = '$tahun'");
-							while ($data = mysql_fetch_array($query))
-							{
-							echo "<h1 style=\"font:Arial, Helvetica, sans-serif; font-size:20px;\">"."<a href=\"viewlpj.php\">".$data['NAMA_PROKER']."</a></h1>";
+	<div class="box-body table-responsive">
+		<table class="table table-hover">
+    		<tbody>
+      			<tr>
+        			<td style="font-size:16px;"><b>Terlaksana</b></td>
+      			</tr>
+      			<tr>
+					<td>
+					<?php
+					if(isset($_POST['tahun'])){
+						$tahun = $_POST['tahun'];
+						$query = mysql_query("SELECT * FROM proker WHERE STATUS_PROKER='Terlaksana' AND date_format(WAKTU_MULAI_PROKER, '%Y') = '$tahun'");
+						while ($data = mysql_fetch_array($query)){
+							echo "<a href=\"viewlpj.php\">".$data['NAMA_PROKER']."</a>";
 							}
+						}
+						?>
+					</td>
+      			</tr>
+				<tr>
+        			<td style="font-size:16px;"><b>Tidak Terlaksana</b></td>
+      			</tr>
+      			<tr>
+        			<td>
+					<?php
+					if(isset($_POST['tahun'])){
+						$tahun = $_POST['tahun'];
+						$query = mysql_query("SELECT * FROM proker WHERE STATUS_PROKER='Tidak Terlaksana' AND date_format(WAKTU_MULAI_PROKER, '%Y') = '$tahun'");
+						while ($data = mysql_fetch_array($query)){
+							echo "<a href=\"viewlpj.php\">".$data['NAMA_PROKER']."</a>";
 							}
-							?>
-							</td>
-						</tr>
-						<tr>
-							<td style="size:landscape; background-color:#C0C0C0;">Tidak Terlaksana</td>
-						</tr>
-						<tr>
-							<td>
-							<?php
-							if(isset($_POST['lihat'])){
-							$tahun = $_POST['tahun'];
-							$query = mysql_query("SELECT * FROM proker WHERE STATUS_PROKER='Tidak Terlaksana' AND date_format(WAKTU_MULAI_PROKER, '%Y') = '$tahun'");
-							while ($data = mysql_fetch_array($query))
-							{
-							echo "<h1 style=\"font:Arial, Helvetica, sans-serif; font-size:20px;\">"."<a href=\"viewlpj.php\">".$data['NAMA_PROKER']."</a></h1>";
-							}
-							}
-							?>
-							</td>
-						</tr>
-						
-                    </thead>  
-				</table>
-				<form action="print.php" method="post">
-				<input type="submit" name="print" value="Print" style="width:75px; height:35px; border:1px dotted #333333; border-radius:4px; -moz-border-radius:4px; font-family:Garamond; font-size:24px; margin-left:10px; cursor:pointer">
-				</form>
-				
-            </div><!-- /.box-body -->
+						}
+						?>
+					</td>
+      			</tr>
+    		</tbody>
+  		</table>
+		<!--<input type="submit" name="print" value="Print" class="btn btn-info">-->
+		
+		<form action="print.php" method="post">
+			<div class="btn-group">
+				<button type="submit" class="btn btn-default" name="print" value="print">Print Semua</button>
+				<button type="submit" class="btn btn-default" name="print" value="print">Print Tahun Terpilih</button>
+			</div>
+		</form>
+	  </div><!-- /.box-body -->
     </div>
   </div>
 </div>
