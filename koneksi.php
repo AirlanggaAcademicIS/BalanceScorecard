b9bc5fb1a584bf33b0ec65ad2a422e7913cd84be
faizakmal <akmalfaiz16@gmail.com>
2016-05-15 09:04:44 +0700

    Update koneksi.php
    DO : Mengetahui sistem tidak terkoneksi ke database
    TO DO: Menampilkan notifikasi data tidak terkoneksi
    Hambatan :menampilkan notifikasi data tidak terkoneksi ke database

--- koneksi.php
+++ koneksi.php
@@ -6,5 +6,13 @@ $host = 'localhost';
   $database = 'pengembangansi';    
       
   $konek_db = mysql_connect($host, $user, $password);      
-  $find_db = mysql_select_db($database) ;  
+ if($konek_db){
+	 $find_db = mysql_select_db($database) ;  
+	}
+	else{
+		echo "<div class='alert alert-danger  alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+               	Database tidak terkoneksi
+                </div>";
+		}
 ?> 
