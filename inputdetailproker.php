8ec3217f6819ad7ab8b7fc8ceac947ede4482a51
ibnuibadillah <Ibnuibadillah@yahoo.co.id>
2016-05-16 16:32:50 +0700

    Update inputdetailproker.php
    sudah bisa menyimpan ke database sesuai nama dan IDnya

--- inputdetailproker.php
+++ inputdetailproker.php
@@ -1,31 +1,15 @@
-<?php include "koneksi.php";
-
-/*echo $_GET['id'];*/
-
-
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include "koneksi.php"?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Balance Score Card</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
-  <!-- Include Required Prerequisites -->
-<script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap/latest/css/bootstrap.css" />
- 
-<!-- Include Date Range Picker -->
-<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
-<title>Untitled Document</title>
 </head>
-
 <body>
 
 <nav class="navbar navbar-inverse">
@@ -39,11 +23,12 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="#">Balance Score Card</a></li>
-        <li class="active"><a href="proker.php">Program Kerja</a></li>
-        <li><a href="viewlpj.php">Laporan Pertanggung Jawaban</a></li>
+        <li ><a href="index.php">Home</a></li>
+        <li><a href="viewbsc.php">Balance Score Card</a></li>
+        <li class="active"><a href="viewproker.php">Program Kerja</a></li>
+        <li><a href="viewlpj.php"> Laporan Pertanggung Jawaban</a></li>
       </ul>
+      </li>
     </div>
   </div>
 </nav>
@@ -51,71 +36,147 @@
 <div class="container-fluid text-center">    
   <div class="row content">
     <div class="col-sm-2 sidenav">
-    <p><a href="proker.php"> <button type="button" class="btn btn-primary btn-block active ">View Proker</button></a></p>
-	<p><a href="#"> <button type="button" class="btn btn-primary btn-block disabled">Input Proker</button></a></p>
-	</div>
-
-
-<div class="col-sm-8 text-left">
-  <h2 class="text-center">Input Program Kerja</h2>
-  <form id="form2" name="form2" method="post" action="inputdetailprokerrefisi1.php">
-  		
+    <p><a href="viewlpj.php"> <button type="button" class="btn btn-primary btn-block active ">View LPJ</button></a></p>
+	<p><button type="button" class="btn btn-primary btn-block disabled">Input LPJ</button></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
+            <h2 class="text-center">Detail Program Kerja</h2>
+      <div class="form-group method="post" action="viewdetailproker.php">
+      		<label class="control-label col-sm-2">ID Proker :</label>
+      <div class="col-sm-10">
+       <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<input type='text'  class='form-control' id='idproker' disabled value='".$data['ID_PROKER']."'><br>";
+			}
+				?>    	
+      </div>
+      <div class="form-group">
+      		<label class="control-label col-sm-2">NIP :</label>
+      <div class="col-sm-10">
+       <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<input type='text'  class='form-control' id='nip' disabled value='".$data['NIP']."'><br>";
+			}
+				?>    	
+      </div>
      <div class="form-group">
-      			<label class="control-label col-sm-2">Nama Proker :</label>
-      		<div class="col-sm-10">
-           	<?php
-			$queri1="Select NAMA_PROKER From proker";
-	  		$hasil1=MySQL_query ($queri1);
-			while ($data = mysql_fetch_array ($hasil1)){
+      		<label class="control-label col-sm-2">Nama Proker :</label>
+      <div class="col-sm-10">
+       <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
 				echo "<input type='text'  class='form-control' id='namaproker' disabled value='".$data['NAMA_PROKER']."'><br>";
 			}
+				?>    	
+      </div>
+    </div>
+    <div class="form-group">
+      	<label class="control-label col-sm-2">Pendahuluan :</label>
+     	<div class="col-sm-10">
+         <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<textarea class='form-control' id='pendahuluan' >".$data['PENDAHULUAN']."</textarea><br>";
+			}
 				?> 
-     		 </div>
-        </div>    
-    <div class="form-group">
-   		<h3>Pendahuluan :</h3>
-		<textarea name="pendahuluan" textarea class="form-control" rows="5" id="comment"></textarea>
+      </div>	
+    </div>
+      <div class="form-group">
+      	<label class="control-label col-sm-2">Latar Belakang :</label>
+     	<div class="col-sm-10">
+         <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<textarea class='form-control' id='latarbelakang' >".$data['LATAR_BELAKANG']."</textarea><br>";
+			}
+				?> 
+      </div>	
+    </div>
+      <div class="form-group">
+      	<label class="control-label col-sm-2">Tujuan :</label>
+     	<div class="col-sm-10">
+        <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<textarea class='form-control' id='tujuan' >".$data['TUJUAN']."</textarea><br>";
+			}
+				?> 
+      </div>	
+    </div>
+      <div class="form-group">
+      	<label class="control-label col-sm-2">Deskripsi :</label>
+     	<div class="col-sm-10">
+         <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<textarea class='form-control' id='deskripsi' >".$data['DESKRIPSI']."</textarea><br>";
+			}
+				?> 
+      </div>	
     </div>
     
     <div class="form-group">
-   		<h3>Latar Belakang :</h3>
-		<textarea name="latarbelakang" textarea class="form-control" rows="5" id="comment"></textarea>
+      	<label class="control-label col-sm-2">Mekanisme dan Rancangan :</label>
+     	<div class="col-sm-10">
+         <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<textarea class='form-control' id='mekanisme_rancangan' >".$data['MEKANISME_DAN_RANCANGAN']."</textarea><br>";
+			}
+				?> 
+      </div>	
     </div>
     
-    
-    <div class="form-group">
-    	<h3>Tujuan :</h3>
-      <textarea name="tujuan" textarea class="form-control" rows="5" id="comment"></textarea>
+
+     <div class="form-group">
+      		<label class="control-label col-sm-2">Anggaran :</label>
+      <div class="col-sm-10">
+         <?php
+			$queri="Select * From proker where ID_PROKER=".$_GET['id'];  //menampikan SEMUA data dari tabel siswa  
+	  		$hasil=MySQL_query ($queri);
+			
+			while ($data = mysql_fetch_array ($hasil)){
+				echo "<input type='text'  class='form-control' id='anggaran' value='".$data['ANGGARAN_DANA']."'><br>";
+			}
+				?>    
+      </div>
     </div>
-    
-    <div class="form-group">
-    	<h3>Mekanisme dan Rancangan :</h3>
-      <textarea name="mekanisme_rancangan" textarea class="form-control" rows="5" id="comment"></textarea>
-    </div>
-        
-        
-		<div class="form-group">
-  		<h3>Anggaran Dana :</h3>
-  		<input name="anggaran" input type="text" class="form-control" id="usr">
-			</div>  	
-    
-       	<h3>Koordinator :</h3>
-    <div class="panel panel-default">
-  		<div class="panel-body">
-    Putratama Geza
-  </div>
-  </div>
-<input type="submit" name="simpan" value="Simpan" class="btn btn-primary" style="margin-top:5px; margin-bottom:5px ;"/>
-  <?php
+    <input type="submit" name="simpan" value="Simpan" class="btn btn-primary"/><br><br><br><br><br>  
+    <?php
 if(isset ($_POST['simpan'])){
 
-echo $_GET['id'];
 
-mysql_query("UPDATE `proker` SET `ANGGARAN_DANA`='".$_POST['anggaran']."',`LATAR_BELAKANG`='".$_POST['latarbelakang']."',`TUJUAN`='".$_POST['tujuan']."',`MEKANISME_DAN_RANCANGAN`='".$_POST['mekanisme_rancangan']."',`PENDAHULUAN`='".$_POST['pendahuluan']."' WHERE ID_PROKER =1");
+
+mysql_query("UPDATE `proker` SET `ANGGARAN_DANA`='".$_POST['anggaran']."',`LATAR_BELAKANG`='".$_POST['latarbelakang']."',`TUJUAN`='".$_POST['tujuan']."',`MEKANISME_DAN_RANCANGAN`='".$_POST['mekanisme_rancangan']."',`PENDAHULUAN`='".$_POST['pendahuluan']."' WHERE ID_PROKER =".$_GET['id']);
 }
 ?>
-</form>
+			</div> 
+		</div>
+	</div>
 </div>
+<div class="navbar navbar-inverse navbar-fixed-bottom">
+      <div class="container">
+      <p class="navbar-text center-block">&#169; Sistem Informasi Universitas Airlangga</p>
+      </div>
+ </div>
 
 </body>
 </html>
