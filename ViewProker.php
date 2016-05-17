c5c6bc500b6d5162e4901dd2e9968a4a8346d5bf
ibnuibadillah <Ibnuibadillah@yahoo.co.id>
2016-05-17 11:30:52 +0700

    Update ViewProker.php
    terdapat link "detail" untuk bisa mengisi detail proker

--- ViewProker.php
+++ ViewProker.php
@@ -1,10 +1,6 @@
-<?php  
-//membuat koneksi ke database  
-include "koneksi.php"; 
-?> 
-
+<?php include "koneksi.php"?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" iostream>
 <head>
   <title>Balance Score Card</title>
   <meta charset="utf-8">
@@ -30,9 +26,9 @@ include "koneksi.php";
         <li ><a href="index.php">Home</a></li>
         <li><a href="viewbsc.php">Balance Score Card</a></li>
         <li class="active"><a href="viewproker.php">Program Kerja</a></li>
-        <li><a href="viewlpj.php"> Laporan Pertanggung Jawaban</a></li>
-		<li><a href="monitoring.php">Monitoring</a></li>
+        <li><a href="viewlpj.php">Laporan Pertanggung Jawaban</a></li>
       </ul>
+      </li>
     </div>
   </div>
 </nav>
@@ -40,49 +36,53 @@ include "koneksi.php";
 <div class="container-fluid text-center">    
   <div class="row content">
     <div class="col-sm-2 sidenav">
-		<img src="images/cinqueterre.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
-		<br></br>
-    	<p><button type="button" class="btn btn-primary btn-block disabled ">View Proker</button></p>
-	 	<p><a href="inputproker.php"><button type="button" class="btn btn-primary btn-block active">Input Proker</button></a></p>
-	 	<p><a href="deleteproker.php"><button type="button" class="btn btn-primary btn-block active">Delete Proker</button></a></p>
+     <p><button type="button" class="btn btn-primary btn-block disabled ">View Proker</button></p>
+	 <p><a href="inputproker.php"><button type="button" class="btn btn-primary btn-block active">Input Proker</button></a></p>
     </div>
+    
     <div class="col-sm-8 text-left"> 
-            <h2 class="text-center">View Program Kerja</h2>
-            <div class="box-body table-responsive">
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>No</th>
-						<th>Nama Program Kerja</th>
-						<th>Nama Koordinator</th>
-						<th>Target Waktu</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<?php
-						$i=0;
-						$query = mysql_query("SELECT p.id_proker, p.nama_proker, k.nama, DATE_FORMAT(p.waktu_mulai_proker, '%d/%m/%Y') AS waktu_mulai_proker, DATE_FORMAT(p.waktu_akhir_proker, '%d/%m/%Y') AS waktu_akhir_proker FROM proker p, karyawan k WHERE p.NIP=k.NIP ");
-						while ($data = mysql_fetch_array($query)){
-							$i++;
-							echo "<td>".$i."</td>
-								  <td>".ucwords($data['nama_proker'])."</td>
-								  <td>".ucwords($data['nama'])."</td>
-								  <td>".$data['waktu_mulai_proker']." - ".$data['waktu_akhir_proker']."</td>
-								  </tr>";}
-							echo '</table>';
-						?>
-					</tr>
-				</tbody>
-			</table>
-			</div><!-- /.box-body -->
-        </div>
-	</div>
+       <h2 class="text-center">Program Kerja</h2>
+   		<div class="box-body table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                        	<th>Nomor</th>
+			    			<th>ID Proker</th>
+                            <th>Nama Proker</th>
+                            <th>Aksi </th>
+                            
+                        </tr>
+                    </thead>  
+                    <?php    
+		// Perintah untuk menampilkan data  
+		$queri="Select * From proker where status_proker='gagal'";  //menampikan SEMUA data dari tabel siswa  
+  		$hasil=MySQL_query ($queri); 
+		$id1=0;   //fungsi untuk SQL  
+  		// perintah untuk membaca dan mengambil data dalam bentuk array  
+			while ($data = mysql_fetch_array ($hasil)){  
+				$id1++;
+ 			echo "      
+        <tr>  
+		<td>".$id1."</td>
+		<td>".$data['ID_PROKER']."</td>  
+        <td>".$data['NAMA_PROKER']."</td>  
+		<td><a href=\"viewdetailproker.php?id=".$data['ID_PROKER']."\">"."Detail</a>"." </td> 
+        </tr>  
+        		";            
+		}    
+ 
+?>  
+				</table>
+    		</div>
+        </div>	
+    </div>
 </div>
 
-<footer class="container-fluid text-center">
-  <p>Sistem Informasi Universitas Airlangga</p>
-</footer>
+<div class="navbar navbar-inverse navbar-fixed-bottom">
+      <div class="container">
+      <p class="navbar-text center-block">&#169; Sistem Informasi Universitas Airlangga</p>
+      </div>
+ </div>
 
 </body>
 </html>
