25b4c4b483b3ffd0ae5695743738eba5bdb7b6fc
rikisar <rikiisardwi@gmail.com>
2016-05-18 09:10:05 +0700

    upload inputbsc
    do: sudah bisa melakukan inputan satu tujuan banyak indikator
    to do: membuat pemberitaun jika ada kolom yang harus diisi
    hambatan: memasukkan indikator satu-satu ke dalam data base dengan id_indikator yang berberda-beda

--- inputbsc.php
+++ inputbsc.php
@@ -15,8 +15,10 @@ $host = 'localhost';
   <title>Balance Score Card</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
@@ -28,18 +30,16 @@ $host = 'localhost';
       <div align="center">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
           <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>                        
-        </button>
+        <span class="icon-bar"></span></button>
       </div>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <div align="center">
         <ul class="nav navbar-nav">
-          <li ><a href="index.php">Home</a></li>
-          <li class="active"><a href="viewbsc.php">Balance Score Card</a></li>
-          <li><a href="viewproker.php">Program Kerja</a></li>
-          <li ><a href="viewlpj.php"> Laporan Pertanggung Jawaban</a></li>
+          <li ><a href="../index.php">Home</a></li>
+          <li class="active"><a href="../viewbsc.php">Balance Score Card</a></li>
+          <li><a href="../viewproker.php">Program Kerja</a></li>
+          <li ><a href="../viewlpj.php"> Laporan Pertanggung Jawaban</a></li>
         </ul>
       </div>
     </div>
@@ -49,9 +49,9 @@ $host = 'localhost';
 <div class="container-fluid text-center">    
   <div class="row content">
     <div class="col-sm-2 sidenav">
-    <p align="center"><a href="viewbsc.php"> <button type="button" class="btn btn-primary btn-block active ">View BSC</button></a></p>
+    <p align="center"><a href="../viewbsc.php"> <button type="button" class="btn btn-primary btn-block active ">View BSC</button></a></p>
 	<p align="center"><button type="button" class="btn btn-primary btn-block disabled">Input BSC</button></p>
-	 <p align="center"><a href="editbsc.php"><button type="button" class="btn btn-primary btn-block active">Edit BSC</button></a></p>
+	 <p align="center"><a href="../editbsc.php"><button type="button" class="btn btn-primary btn-block active">Edit BSC</button></a></p>
 	</p>
 	
   <p align="center">&nbsp; </p>
@@ -63,7 +63,8 @@ $host = 'localhost';
 	        <div align="left"><strong>Prespeektif</strong>
               </label>
               </div>
-	        <label></label>
+	        <label>
+			</label>
      
       
         
@@ -88,12 +89,19 @@ $host = 'localhost';
 			<div align="left">Indikator</div>
 			</label>
 		  <div align="left">
-		   
-			  <input name="Indikator1" class="form-control" > 
-			  </p>
-			  
-			  
-			  
+		  
+			   <input name="Indikator1" class="form-control" > 
+			   </p>
+			
+			   <input name="Indikator2" class="form-control" > 
+			 </p>
+			 <input name="Indikator3" class="form-control" > 
+			   </p>
+			   <input name="Indikator4" class="form-control" > 
+			   </p>
+			   <input name="Indikator5" class="form-control" > 
+			   </p>
+		</td>
 			  
 			  
 			  
@@ -108,7 +116,7 @@ $host = 'localhost';
   
   <?php 
 		if(isset($_POST['simpan']))
-		{if(($_POST['Tujuan']!=NULL&&$_POST['Perspektif']!="")||$_POST['Indikator1']!=NULL)
+		{if((((($_POST['Tujuan']!=NULL&&$_POST['Perspektif']!=""&&$_POST['Indikator1']!=NULL)||$_POST['Indikator2']!=NULL)||$_POST['Indikator3']!=NULL)||$_POST['Indikator4']!=NULL)||$_POST['Indikator5']!=NULL)
 			{
 			
 				$sql = "INSERT INTO tujuan (TUJUAN_ORGANISASI, PERSPEKTIF) VALUES ('".$_POST['Tujuan']."','".$_POST['Perspektif']."')";
@@ -121,8 +129,19 @@ $host = 'localhost';
 			if($_POST['Indikator1']!=NULL){	
 			$sql = "INSERT INTO indikator_tujuan (NAMA_INDIKATOR_TUJUAN,ID_TUJUAN) VALUES ('".$_POST['Indikator1']."',".$data2['ID_TUJUAN'].")";
 				mysql_query($sql);
-				
-			}}
+				if($_POST['Indikator2']!=NULL){	
+			$sql = "INSERT INTO indikator_tujuan (NAMA_INDIKATOR_TUJUAN,ID_TUJUAN) VALUES ('".$_POST['Indikator2']."',".$data2['ID_TUJUAN'].")";
+				mysql_query($sql);
+				if($_POST['Indikator3']!=NULL){	
+			$sql = "INSERT INTO indikator_tujuan (NAMA_INDIKATOR_TUJUAN,ID_TUJUAN) VALUES ('".$_POST['Indikator3']."',".$data2['ID_TUJUAN'].")";
+				mysql_query($sql);
+				if($_POST['Indikator4']!=NULL){	
+			$sql = "INSERT INTO indikator_tujuan (NAMA_INDIKATOR_TUJUAN,ID_TUJUAN) VALUES ('".$_POST['Indikator4']."',".$data2['ID_TUJUAN'].")";
+				mysql_query($sql);
+				if($_POST['Indikator5']!=NULL){	
+			$sql = "INSERT INTO indikator_tujuan (NAMA_INDIKATOR_TUJUAN,ID_TUJUAN) VALUES ('".$_POST['Indikator5']."',".$data2['ID_TUJUAN'].")";
+				mysql_query($sql);
+				}}}}}}
 			else
 			{
 			
