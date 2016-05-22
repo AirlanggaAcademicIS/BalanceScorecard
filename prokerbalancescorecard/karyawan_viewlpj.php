3faf1bc91bd71abe1be848dbf6e5ce803cc7f28f
faizakmal <akmalfaiz16@gmail.com>
2016-05-22 13:36:53 +0700

    Update karyawan_viewlpj.php

--- prokerbalancescorecard/karyawan_viewlpj.php
+++ prokerbalancescorecard/karyawan_viewlpj.php
@@ -28,6 +28,9 @@
         <li><a href="karyawan_viewproker.php">Program Kerja</a></li>
         <li class="active"><a href="karyawan_viewlpj.php">Laporan Pertanggung Jawaban</a></li>
       </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
       </li>
     </div>
   </div>
