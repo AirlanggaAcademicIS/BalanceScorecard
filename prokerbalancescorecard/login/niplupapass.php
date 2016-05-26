24f96bd391018c5fca0b1db002526b07d2cafd6f
ibnuibadillah <Ibnuibadillah@yahoo.co.id>
2016-05-26 18:12:44 +0700

    Update niplupapass.php
    sudah bisa tersambung ke database sesuai NIP yang di tulis, yang belum hanya membbenahi tampilannya.

--- prokerbalancescorecard/login/niplupapass.php
+++ prokerbalancescorecard/login/niplupapass.php
@@ -1,3 +1,4 @@
+<?php include "koneksi.php";?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -25,7 +26,7 @@
         <li ><a href="index.php">Home</a></li>
         <li><a href="viewbsc.php">Balance Score Card</a></li>
         <li class="active"><a href="viewproker.php">Program Kerja</a></li>
-        <li><a href="viewlpj.php">Laporan Pertanggung Jawaban</a></li>
+        <li><a href="viewlpj.php"> Laporan Pertanggung Jawaban</a></li>
       </ul>
       </li>
     </div>
@@ -35,13 +36,35 @@
 <div class="container-fluid text-center">    
   <div class="row content">
     <div class="col-sm-2 sidenav">
-     <p><button type="button" class="btn btn-primary btn-block disabled ">View Proker</button></p>
-	 <p><a href="inputproker.php"><button type="button" class="btn btn-primary btn-block active">Input Proker</button></a></p>
+    <p><a href="viewlpj.php"> <button type="button" class="btn btn-primary btn-block active ">View LPJ</button></a></p>
+	<p><button type="button" class="btn btn-primary btn-block disabled">Input LPJ</button></p>
     </div>
-    
     <div class="col-sm-8 text-left"> 
-<input type='text'  class='form-control' name='resetpass'/>
-<input type="submit" name="cek" value="Next" class="btn btn-primary"/>
-</div>
+            <h2 class="text-center">Masukkan NIP Anda</h2>
+    
+    <form action="niplupapass.php" method="post">
+    
+    <div class="form-group">
+      		<label class="control-label col-sm-1">NIP :</label>
+      
+         <input name="NIP/NIK" type="text" id="NIP/NIK" onBlur="MM_validateForm('NIP/NIK','','NisNum');return document.MM_returnValue" size="25" required="required">
+      
+    <br />
+    <br />
+    <p onfocus="MM_validateForm('NIP/NIK','','RisNum');return document.MM_returnValue">
+         <input type="submit" name="next" value="Next" class="btn btn-primary" ; mm_validateform('nip/nik','','risnum');return document.mm_returnvalue">
+       </p>  
+       </div>
+       </form>
+       <?php
+	   if (isset($_POST['next'])){
+	   $sql="select * from karyawan where NIP='".$_POST['NIP/NIK']."'";
+	   $query = mysql_query("select * from karyawan where NIP='".$_POST['NIP/NIK']."'");
+	   $rows = mysql_num_rows($query);
+	   if ($rows==1){
+		header("location: lupapass.php?NIP=".$_POST['NIP/NIK']);
+	   }
+	   }
+	   ?>
 </body>
 </html>
