30e93dc7a0479f87a5d6c012aa2b8d3147a9e9f8
Faiz Haidar Satriani Adli <faizhaidarsa@gmail.com>
2016-06-06 23:48:48 +0700

    Update karyawan_viewproker.php

--- prokerbalancescorecard/karyawan_viewproker.php
+++ prokerbalancescorecard/karyawan_viewproker.php
@@ -49,41 +49,135 @@ include "koneksi.php";
     </div>
     <div class="col-sm-8 text-left"> 
             <h2 class="text-center">View Program Kerja</h2>
-            <div class="box-body table-responsive">
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>No</th>
-						<th>Nama Program Kerja</th>
-						<th>Nama Koordinator</th>
-						<th>Target Waktu</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<?php
-						$i=0;
-						$query = mysql_query("SELECT p.id_proker, p.nama_proker, k.nama, DATE_FORMAT(p.waktu_mulai_proker, '%d/%m/%Y') AS waktu_mulai_proker, DATE_FORMAT(p.waktu_akhir_proker, '%d/%m/%Y') AS waktu_akhir_proker FROM proker p, karyawan k WHERE p.NIP=k.NIP ");
-						while ($data = mysql_fetch_array($query)){
-							$i++;
-							echo "<td>".$i."</td>
-								  <td>".ucwords($data['nama_proker'])."</td>
-								  <td>".ucwords($data['nama'])."</td>
-								  <td>".$data['waktu_mulai_proker']." - ".$data['waktu_akhir_proker']."</td>
-								  </tr>";}
-							echo '</table>';
-						?>
-					</tr>
-				</tbody>
-			</table>
-			</div><!-- /.box-body -->
-        </div>
-	</div>
+            <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#belumterlaksana">Belum Terlaksana</a></li>
+    <li><a data-toggle="tab" href="#terlaksana">Terlaksana</a></li>
+    <li><a data-toggle="tab" href="#tidakterlaksana">Tidak Terlaksana</a></li>
+  	</ul>
+<div class="tab-content">
+    <div id="terlaksana" class="tab-pane fade in active"><br>
+      <div class="box-body table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                        	<th>Nomor</th>
+			    			<th>ID Proker</th>
+                            <th>Nama Proker</th>
+                            <th>Waktu</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>  
+                    <?php    
+     
+		// Perintah untuk menampilkan data  
+		$queri="Select * From proker where status_proker='Terlaksana' and nip='".$_SESSION['login_user']."'";  //menampikan SEMUA data dari tabel siswa  
+  		$hasil=MySQL_query ($queri);  
+		$id = 0;    //fungsi untuk SQL  
+  		// perintah untuk membaca dan mengambil data dalam bentuk array  
+			while ($data = mysql_fetch_array ($hasil)){  
+				$id++; 	
+ 			echo "      
+        <tr>  
+			<td>".$id."</td>
+			<td>".$data['ID_PROKER']."</td>  
+        	<td>".$data['NAMA_PROKER']."</td>  
+        	<td>".$data['WAKTU_MULAI_PROKER']." s/d ".$data['WAKTU_AKHIR_PROKER']."</td>  
+        	<td>".$data['STATUS_PROKER']."</td>  
+			<td><a href=\"karyawan_viewdetailproker.php?id=".$data['ID_PROKER']."\">"."Detail</a> "." || <a href=\"karyawan_viewdetaillpj.php?id=".$data['ID_LPJ']."\">"."LPJ</a>"."</td>  
+        </tr>  
+        		";            
+		}    
+ 
+?>  
+				</table>
+    		</div>
+            
+    </div>
+    <div id="tidakterlaksana" class="tab-pane fade"><br>
+   		<div class="box-body table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                        	<th>Nomor</th>
+			    			<th>ID Proker</th>
+                            <th>Nama Proker</th>
+                            <th>Waktu</th>
+                            <th>Status</th>
+                            <th>Aksi </th>
+                            
+                        </tr>
+                    </thead>  
+                    <?php    
+		// Perintah untuk menampilkan data  
+		$queri="Select * From proker where status_proker='Tidak terlaksana' and nip='".$_SESSION['login_user']."'";  //menampikan SEMUA data dari tabel siswa  
+  		$hasil=MySQL_query ($queri); 
+		$id1=0;   //fungsi untuk SQL  
+  		// perintah untuk membaca dan mengambil data dalam bentuk array  
+			while ($data = mysql_fetch_array ($hasil)){  
+				$id1++;
+ 			echo "      
+        <tr>  
+		<td>".$id1."</td>
+		<td>".$data['ID_PROKER']."</td>  
+        <td>".$data['NAMA_PROKER']."</td>  
+        <td>".$data['WAKTU_MULAI_PROKER']." s/d ".$data['WAKTU_AKHIR_PROKER']."</td>  
+        <td>".$data['STATUS_PROKER']."</td> 
+		<td><a href=\"karyawan_viewdetailproker.php?id=".$data['ID_PROKER']."\">"."Detail</a>"." || <a href=\"karyawan_viewnotif.php?id=".$data['ID_PROKER']."\">"."Notifikasi</a>"."</td>  
+        </tr>  
+        		";            
+		}    
+ 
+?>  
+				</table>
+    		</div>
+        </div>	
+    <div id="belumterlaksana" class="tab-pane fade"><br>
+   		<div class="box-body table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                        	<th>Nomor</th>
+			    			<th>ID Proker</th>
+                            <th>Nama Proker</th>
+                            <th>Waktu</th>
+                            <th>Status</th>
+                            <th>Aksi </th>
+                            
+                        </tr>
+                    </thead>  
+                    <?php    
+		// Perintah untuk menampilkan data  
+		$queri="Select * From proker where status_proker='Belum terlaksana' and nip='".$_SESSION['login_user']."'";  //sql yang diganti disini iz
+  		$hasil=MySQL_query ($queri);  
+		$id1=0;   //fungsi untuk SQL  
+  		// perintah untuk membaca dan mengambil data dalam bentuk array  
+			while ($data = mysql_fetch_array ($hasil)){  
+				$id1++;
+ 			echo "      
+        <tr>  
+		<td>".$id1."</td>
+		<td>".$data['ID_PROKER']."</td>  
+        <td>".$data['NAMA_PROKER']."</td>  
+        <td>".$data['WAKTU_MULAI_PROKER']." s/d ".$data['WAKTU_AKHIR_PROKER']."</td>  
+        <td>".$data['STATUS_PROKER']."</td> 
+		<td><a href=\"karyawan_inputdetailproker.php?id=".$data['ID_PROKER']."\">"."Detail</a>"." || <a href=\"karyawan_inputnotif1.php?id=".$data['ID_PROKER']."\">"."Notifikasi</a>"." || <a href=\"karyawan_inputlpj.php?id=".$data['ID_PROKER']."\">"."LPJ</a>"."</td>  
+        </tr>  
+        		";            
+		}    
+ 
+?>  
+				</table>
+    		</div>
+        </div>	
+    </div>
 </div>
 
-<footer class="container-fluid text-center">
-  <p>Sistem Informasi Universitas Airlangga</p>
-</footer>
+<div class="navbar navbar-inverse navbar-fixed-bottom">
+      <div class="container">
+      <p class="navbar-text center-block">&#169; Sistem Informasi Universitas Airlangga</p>
+      </div>
+ </div>
 
 </body>
 </html>
