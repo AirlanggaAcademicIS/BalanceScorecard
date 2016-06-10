67a248deb1cf14af120dd957462a08de048cb268
faizakmal <akmalfaiz16@gmail.com>
2016-06-10 09:24:59 +0700

    Update karyawan_viewlpj.php

--- prokerbalancescorecard/karyawan_viewlpj.php
+++ prokerbalancescorecard/karyawan_viewlpj.php
@@ -77,7 +77,7 @@
  <?php
 if(isset($_POST['tahun']))
 if($_POST['tahun']!="Tahun"){	
-$queri="Select distinct l.id_lpj, p.nama_proker, l.evaluasi, l.keberlanjutan From laporan_pertanggung_jawaban l, proker p where l.id_proker=p.id_proker and YEAR(WAKTU_MULAI_PROKER) = \"".$_POST['tahun']."\"";
+$queri="Select distinct l.id_lpj, p.nama_proker, l.evaluasi, l.keberlanjutan From laporan_pertanggung_jawaban l, proker p where l.id_proker=p.id_proker and YEAR(WAKTU_MULAI_PROKER) = \"".$_POST['tahun']."\" and p.nip='".$_SESSION['login_user']."'";
 $hasil=MySQL_query ($queri);    //fungsi untuk SQL  
 $id = 0;
 while ($data = mysql_fetch_array ($hasil)){  
