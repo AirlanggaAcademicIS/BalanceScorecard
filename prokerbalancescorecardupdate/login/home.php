4b33bc8a2d6dfeaded4782e6017be85e3b096863
Faiz Haidar Satriani Adli <faizhaidarsa@gmail.com>
2016-06-26 17:15:24 +0700

    Add files via upload

---
+++ prokerbalancescorecardupdate/login/home.php
@@ -0,0 +1,54 @@
+<?php
+include('session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Balance Score Card</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="viewbsc.php">Balance Score Card</a></li>
+        <li><a href="viewproker.php">Program Kerja</a></li>
+        <li><a href="viewlpj.php">Laporan Pertanggung Jawaban</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+	<b id="welcome">Welcome <?php echo $login_session; ?> ! </b><br>
+	<b id="welcome">NIP : <?php echo $login_session1; ?></b>
+    </div>
+    <div class="col-sm-8 text-left"> 
+            <h2 class="text-center">Welcome</h2>
+            <p>Selamat datang di portal balance score card. Silahkan memilih menu yang tersedia</p>
+            </div>
+        </div>	 
+    </div>
+
+<footer class="container-fluid text-center">
+  <p>Sistem Informasi Universitas Airlangga</p>
+</footer>
+
+</body>
+</html>
